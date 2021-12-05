8711761d79677257f202abfe47b232c50fda75d9
이현승 <gesal03@naver.com>
2021-12-05 11:30:00 +0000

    4153 직각삼각형

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void sort(int *a, int *b, int *c){
+	int tmp;
+	if( *a > *b ){
+		tmp=*a;
+		*a=*b;
+		*b=tmp;
+	}
+	if(*b>*c){
+		tmp=*b;
+		*b=*c;
+		*c=tmp;
+	}
+	if( *a > *b ){
+		tmp=*a;
+		*a=*b;
+		*b=tmp;
+	}
+}
+
 int main() {
-	cout<<"hello world";
-	return 0;
+	while(1){
+		int a,b,c;
+		cin>>a>>b>>c;
+		if(a==0 &&b==0&&c==0)
+			break;
+		
+		sort(&a,&b,&c);
+	
+		if(c*c == a*a + b*b)
+			cout<<"right"<<endl;	
+		else
+			cout<<"wrong"<<endl;
+	}
 }
